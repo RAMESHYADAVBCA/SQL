c73ac570c5cf842380b88def8337dfc3f0a353d2
RAMESHYADAVBCA <ry275886@gmail.com>
2025-01-01 13:04:03 +0530

    insert

--- customers.sql
+++ customers.sql
@@ -2,4 +2,5 @@ create database school;
 use school;
 create table customers(customer_id INT PRIMARY KEY, customer_Name varchar(225), address varchar(225), city varchar(225), country varchar(225));
 insert into customers values(1, 'Ramesh Yadav', 'Noida', 'Noida', 'india' );
+insert into customers values(1, 'Ramakant', 'varanasi', 'varanasi', 'india' );
 select * from customers;
