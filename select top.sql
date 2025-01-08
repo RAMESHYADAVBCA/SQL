2fd12e12e52e3beca5641fe94d63bc13b4e11f4a
RAMESHYADAVBCA <ry275886@gmail.com>
2025-01-08 15:27:50 +0530

    (Select top)cls

---
+++ select top.sql
@@ -0,0 +1,14 @@
+use deno;
+create table customers10(customer_id INT primary key, customer_name varchar(255), contact_name varchar(255), address varchar(255), city varchar(255),postalcode varchar(255), country varchar(255));
+ 
+ insert into customers10 values(1, 'Rvi', 'Rvi Kumar','jamuanv', 'sonbhadra', '10201', 'India');
+ insert into customers10 values(5,'Alfreds Futterkiste','Ram' , 'obere str. 57', 'Berlin', '12209','Japan');
+ 
+ insert into customers10 values(2,'Ramesh', 'Ramesh yadav','Avda. de la constitucion 2222', 'Mexcio D.F', '05021', 'Mexico');
+ insert into customers10 values(3,'Antonio Moreno Taqueria','Antonio Moreno','Mataderos 2312','Mexico D.F','05023','Mexico');
+ insert into customers10 values(4,'Around the Horn','Thomas Hardy', '120 Hanover Sq.','London','WA1 1DP', 'UK');
+ select * from customers10;
+ 
+ 
+ /* LIMIT */
+ SELECT   * FROM customers10 LIMIT 3;
